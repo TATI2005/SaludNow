7cda9ced685cd58d7e5e9ce0f5d09533da98e653
TATI2005 <sernatatiana885@gmail.com>
2026-05-24 19:28:12 -0500

    Fix AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,38 +3,33 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\Comentario;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
+    public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         view()->composer('*', function ($view) {
             $comentarios = Comentario::latest()->get();
             $view->with('comentarios', $comentarios);
         });
 
         Carbon::setLocale('es');
+
         \View::composer('Administracion.modal-mostrar-post', function ($view) {
             $articulos = \DB::connection('mongodb')->table('blogs')->orderBy('created_at', 'desc')->get();
             $view->with('articulos', $articulos);
         });
-
-         if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
-
     }
 }
