7f60e65243bbed611ee0976975815fb9333d35f5
TATI2005 <sernatatiana885@gmail.com>
2026-05-24 19:02:13 -0500

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,5 +32,9 @@ class AppServiceProvider extends ServiceProvider
             $view->with('articulos', $articulos);
         });
 
+         if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
     }
 }
